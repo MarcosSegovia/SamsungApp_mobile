964739683bdeea636c2104ee8603291e88cf202c
Marcos Segovia <velozmarkdrea@gmail.com>
2015-01-25 18:17:02 +0100

    gameLogic added

--- js/connection.js
+++ js/connection.js
@@ -9,6 +9,8 @@ var cardsNumber;
 var actualChannel;
 var table;
 var tablenumberCard;
+var cardsSelected;
+var numCardsSelected;
 
 
 $(document).ready(function(){
@@ -17,13 +19,15 @@ $(document).ready(function(){
 	actualChannel = null;
 	cardsPlayer = new Array();
 	table = new Array();
+	cardsSelected = new Array();
+	numCardsSelected = 0;
 	cardsNumber = 0;
 	tablenumberCard = 0;
 
 	$(document).on("click", "#btnSubmit", findDeviceByPin);
 	$(document).on("click", "#select_button", selectCards);
 	$(document).on("click", "#send_button", checkCardsSelected);
-	$(document).on("click", "#end_button", sendMessageHost);
+	$(document).on("click", "#end_button", endTurnPlayer);
 
 });
 
@@ -87,7 +91,7 @@ $(document).ready(function(){
 	    	{
 	    		//Cambiamos la escena a la de juego.
 	    		$('body').empty();
-	    		$('body').append('<section><button id="button">SELECT</button><button id="send_button">SEND</button><button id="end_button">END TURN</button><img id="player_icon" src="img/'+playerColour+'.png"></section>');
+	    		$('body').append('<section><button id="select_button">SELECT</button><button id="send_button">SEND</button><button id="end_button">END TURN</button><img id="player_icon" src="img/'+playerColour+'.png"></section>');
 	    		$('body').append('<div id="playerTurn"><font size="5" color="red"><b>WAIT FOR YOUR TURN...</b></font></div>')
 	    		$('body').append('<div id="cardsrow"></div>');
 	    		
@@ -114,7 +118,7 @@ $(document).ready(function(){
 
 	    		//Añadimos la carta a nuestra mano
 
-	    		$('#cardsrow').after('<img id="'+cardsPlayer[cardsNumber]['colour']+cardsPlayer[cardsNumber]['number']+'" class="ficha" src="img/cards/'+cardsPlayer[cardsNumber]['colour']+cardsPlayer[cardsNumber]['number']+'.png">');
+	    		$('#cardsrow').after('<img id="'+cardsPlayer[cardsNumber]['colour']+'-'+cardsPlayer[cardsNumber]['number']+'" class="ficha" src="img/cards/'+cardsPlayer[cardsNumber]['colour']+cardsPlayer[cardsNumber]['number']+'.png">');
 
 	    		cardsNumber++;
 	    	}
@@ -135,6 +139,26 @@ $(document).ready(function(){
 
 	};
 
+	endTurnPlayer = function()
+	{
+		//Limpiamos el array de cartas seleccionadas y deseleccionamos cartas
+		cardsSelected.splice(0,cardsSelected.length);
+		numCardsSelected = 0;
+		//Limpiamos el array de la mesa
+		table.splice(0, table.length);
+		tablenumberCard = 0;
+		//Desbindeamos el click del SELECT
+		$(document).off( "click", "#select_button", selectCards);
+
+		for(var i=0; i<cardsNumber; i++)
+		{
+			$("#"+cardsPlayer[i]['colour']+'-'+cardsPlayer[i]['number']+"").css("border", "none");
+		}
+		
+		//Enviamos el fin de turno al host
+		sendMessageHost();
+	}
+
 	sendMessageHost = function()
 	{
 		var message = {
@@ -146,7 +170,39 @@ $(document).ready(function(){
 		$('#playerTurn').empty();
 	    $('#playerTurn').append('<font size="5" color="red"><b>WAIT FOR YOUR TURN...</b></font>');
 	}
-	
+
+	selectCards = function()
+	{
+		$(document).on("click", ".ficha", function(){
+
+			console.log('NUMERO CARTAS SELECCTIONADAS: '+cardsSelected.length);
+			var cardId = $(this).attr("id");
+
+				if ($("#"+cardId+"").css("border-top-style") == "solid") 
+				{
+					$("#"+cardId+"").css("border", "none");
+					var containerId = cardId.split("-");
+					for(var i=0; i<numCardsSelected; i++)
+					{
+						if(cardsSelected[i]['colour'] == containerId[0] && cardsSelected[i]['number'] == containerId[1])
+						{
+							cardsSelected.splice(i,1);
+							numCardsSelected--;
+						}
+					}
+				}
+				else
+				{
+					$("#"+cardId+"").css("border", "2px solid yellow");
+					var containerId = cardId.split("-");
+					cardsSelected[numCardsSelected] = new Array();
+					cardsSelected[numCardsSelected]['colour'] = containerId[0];
+					cardsSelected[numCardsSelected]['number'] = containerId[1];
+					numCardsSelected++;
+				}
+		});
+					
+	}
 
 	//Algoritmo de comparación con la mesa.
 	checkCardsSelected = function()
