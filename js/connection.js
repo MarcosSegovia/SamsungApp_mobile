5cbf46b0b75dd5b53b63bc14271213276f4c85f8
Marcos Segovia <velozmarkdrea@gmail.com>
2015-01-25 23:21:10 +0100

    App finished ! =D

--- js/connection.js
+++ js/connection.js
@@ -11,6 +11,7 @@ var table;
 var tablenumberCard;
 var cardsSelected;
 var numCardsSelected;
+var buttonEnabled;
 
 
 $(document).ready(function(){
@@ -23,6 +24,7 @@ $(document).ready(function(){
 	numCardsSelected = 0;
 	cardsNumber = 0;
 	tablenumberCard = 0;
+	buttonEnabled = false;
 
 	$(document).on("click", "#btnSubmit", findDeviceByPin);
 	$(document).on("click", "#select_button", selectCards);
@@ -34,7 +36,6 @@ $(document).ready(function(){
 	onError = function(e){
 		alert(error.message);
 	}
-
 
 	findDeviceByPin = function(){
 	    channelId = "com.samsung.multiscreen.rubikapp"; 
@@ -60,17 +61,6 @@ $(document).ready(function(){
 	    actualChannel = channel;
 	    $('fieldset').append('<p id="connection"><b><font color="green">CONNECTED</font></b></p>');
 
-	    // Wire up some event handlers
-	    /*if(endTurn)
-	    {
-	    	var message = {
-				type:"client command",
-				text: "Finish"
-			};
-			channel.send(JSON.stringify(message), "host");
-			endTurn=false;
-	    }*/
-
 	    channel.on("disconnect", function(myClient){
 
 	    });
@@ -95,13 +85,14 @@ $(document).ready(function(){
 	    		$('body').append('<div id="playerTurn"><font size="5" color="red"><b>WAIT FOR YOUR TURN...</b></font></div>')
 	    		$('body').append('<div id="cardsrow"></div>');
 	    		
-		        $('link[href="styleSincro.css"]').attr('href','style.css');
+		        $('link[href="css/styleSincro.css"]').attr('href','css/style.css');
 	    	}
 
 	    	if(message.text == "Go")
 	    	{
 	    		$('#playerTurn').empty();
 	    		$('#playerTurn').append('<font size="5" color="green"><b>IT IS YOUR TURN</b></font>');
+	    		buttonEnabled = true;
 	    	}
 
 	    	if(message.text == "Colour")
@@ -141,34 +132,58 @@ $(document).ready(function(){
 
 	endTurnPlayer = function()
 	{
-		//Limpiamos el array de cartas seleccionadas y deseleccionamos cartas
-		cardsSelected.splice(0,cardsSelected.length);
-		numCardsSelected = 0;
-		//Limpiamos el array de la mesa
-		table.splice(0, table.length);
-		tablenumberCard = 0;
-		//Desbindeamos el click del SELECT
-		$(document).off( "click", "#select_button", selectCards);
-
-		for(var i=0; i<cardsNumber; i++)
+		if(buttonEnabled)
 		{
-			$("#"+cardsPlayer[i]['colour']+'-'+cardsPlayer[i]['number']+"").css("border", "none");
+			//Limpiamos el array de cartas seleccionadas y deseleccionamos cartas
+			cardsSelected.splice(0,cardsSelected.length);
+			numCardsSelected = 0;
+
+			for(var i=0; i<cardsNumber; i++)
+			{
+				$("#"+cardsPlayer[i]['colour']+'-'+cardsPlayer[i]['number']+"").css("border", "none");
+			}
+
+			//Limpiamos el array de la mesa
+			table.splice(0, table.length);
+			tablenumberCard = 0;
+
+			//Enviamos el fin de turno al host
+			sendMessageHost(true);
 		}
-		
-		//Enviamos el fin de turno al host
-		sendMessageHost();
 	}
 
-	sendMessageHost = function()
+	sendMessageHost = function(getCard)
 	{
 		var message = {
 			type:"client command",
-			text: "EndTurn"
+			text: "EndTurn",
+			card: getCard
 		};
 
 		actualChannel.send(JSON.stringify(message), "host");
 		$('#playerTurn').empty();
 	    $('#playerTurn').append('<font size="5" color="red"><b>WAIT FOR YOUR TURN...</b></font>');
+	    buttonEnabled = false;
+	}
+
+	sendMessageVictoryToHost = function()
+	{
+		var message = {
+			text: "WIN"
+		};
+
+		actualChannel.send(JSON.stringify(message), "host");
+	}
+
+	sendCardHost = function(number, colour)
+	{
+		var message = {
+			text: "Card",
+			number: number,
+			colour: colour
+		};
+
+		actualChannel.send(JSON.stringify(message), "host");
 	}
 
 	selectCards = function()
@@ -182,6 +197,7 @@ $(document).ready(function(){
 				{
 					$("#"+cardId+"").css("border", "none");
 					var containerId = cardId.split("-");
+
 					for(var i=0; i<numCardsSelected; i++)
 					{
 						if(cardsSelected[i]['colour'] == containerId[0] && cardsSelected[i]['number'] == containerId[1])
@@ -207,5 +223,166 @@ $(document).ready(function(){
 	//Algoritmo de comparación con la mesa.
 	checkCardsSelected = function()
 	{
+		if(buttonEnabled)
+		{
+			//2 opciones de tirada:
+			//1- Podemos inserir cartas siempre que sean continuas a las de la mesa.
+			//2- Podemos inserir qualquier carta siempre que sean 2 o más y sean continuas entre ellas.
+			var success = false;
+			var deleteCards = new Array();
+			var numberdeletes = 0;
+			
+			//Caso 2
+			for(var i = 0; i<numCardsSelected; i++)
+			{
+				var actualCard = new Array();
+				actualCard['number'] = cardsSelected[i]['number'];
+				actualCard['colour'] = cardsSelected[i]['colour'];
 
+
+				console.log('number: '+actualCard['number']);
+
+				for(var j=0; j<numCardsSelected; j++)
+				{
+					
+					if(actualCard['colour'] == cardsSelected[j]['colour'])
+					{
+
+						if(actualCard['number'] == parseFloat(cardsSelected[j]['number'])+parseFloat(1))
+						{
+							//Enviamos la carta
+							sendCardHost(actualCard['number'], actualCard['colour']);
+
+							//La eliminamos de nuestra mano
+							for(var card = 0; card < cardsNumber; card++ )
+							{
+								if(cardsPlayer[card]['number']==actualCard['number'])
+								{
+									if(cardsPlayer[card]['colour']==actualCard['colour'])
+									{
+										cardsPlayer.splice(card,1);
+										cardsNumber--;
+									}
+								}
+							}
+							$("#"+actualCard['colour']+'-'+actualCard['number']+"").remove();
+							success = true;
+							deleteCards[numberdeletes] = i;
+							numberdeletes++;
+							break;
+						}
+
+						if(actualCard['number'] == (cardsSelected[j]['number']-1))
+						{
+							//Enviamos la carta
+							sendCardHost(actualCard['number'], actualCard['colour']);
+							//La eliminamos de nuestra mano
+							for(var card = 0; card < cardsNumber; card++ )
+							{
+								if(cardsPlayer[card]['number']==actualCard['number'])
+								{
+									if(cardsPlayer[card]['colour']==actualCard['colour'])
+									{
+										cardsPlayer.splice(card,1);
+										cardsNumber--;
+									}
+								}
+							}
+							$("#"+actualCard['colour']+'-'+actualCard['number']+"").remove();
+							success = true;
+							deleteCards[numberdeletes] = i;
+							numberdeletes++;
+							break;
+						}
+					}
+				}
+			}
+
+			//Borramos las cartas enviadas en el caso 2
+			for(var del=0; del<numberdeletes; del++)
+			{
+				cardsSelected.splice(deleteCards[del],1);
+				numCardsSelected--;
+			}
+
+			//Caso 1
+			for(var i = 0; i < numCardsSelected; i++)
+			{
+				var actualCard = new Array();
+				actualCard['number'] = cardsSelected[i]['number'];
+				actualCard['colour'] = cardsSelected[i]['colour'];
+
+				for(var j = 0; j < tablenumberCard; j++)
+				{
+					if(actualCard['colour'] == table[j]['colour'])
+					{
+						if(actualCard['number'] == parseFloat(table[j]['number'])+parseFloat(1))
+						{
+							//Enviamos la carta
+							sendCardHost(actualCard['number'], actualCard['colour']);
+							//La eliminamos de nuestra mano
+							for(var card = 0; card < cardsNumber; card++ )
+							{
+								if(cardsPlayer[card]['number']==actualCard['number'])
+								{
+									if(cardsPlayer[card]['colour']==actualCard['colour'])
+									{
+										cardsPlayer.splice(card,1);
+										cardsNumber--;
+									}
+								}
+							}
+							$("#"+actualCard['colour']+'-'+actualCard['number']+"").remove();
+							success = true;
+						}
+
+						if(actualCard['number'] == (table[j]['number']-1))
+						{
+							//Enviamos la carta
+							sendCardHost(actualCard['number'], actualCard['colour']);
+							//La eliminamos de nuestra mano
+							for(var card = 0; card < cardsNumber; card++ )
+							{
+								if(cardsPlayer[card]['number']==actualCard['number'])
+								{
+									if(cardsPlayer[card]['colour']==actualCard['colour'])
+									{
+										cardsPlayer.splice(card,1);
+										cardsNumber--;
+									}
+								}
+							}
+							$("#"+actualCard['colour']+'-'+actualCard['number']+"").remove();
+							success = true;
+						}
+					}
+				}
+			}
+
+			//Si nos quedamos sin cartas, hemos ganado la partida.
+			if(cardsNumber == 0)
+			{
+				sendMessageVictoryToHost();
+			}
+
+			//Si hemos podido enviar alguna de las cartas seleccionadas.
+			if(success)
+			{
+				//Limpiamos el array de cartas seleccionadas y deseleccionamos cartas
+				cardsSelected.splice(0,cardsSelected.length);
+				numCardsSelected = 0;
+				for(var i = 0; i < cardsNumber; i++)
+				{
+					$("#"+cardsPlayer[i]['colour']+'-'+cardsPlayer[i]['number']+"").css("border", "none");
+				}
+
+				//Limpiamos el array de la mesa
+				table.splice(0, table.length);
+				tablenumberCard = 0;
+
+
+				//Informamos al host de que hemos terminado el turno
+				sendMessageHost(false);
+			}
+		}
 	}
